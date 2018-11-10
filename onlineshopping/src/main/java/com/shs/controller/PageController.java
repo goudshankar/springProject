1b367b1a0101f80b4d70627cc478108460d491a8
shankar <shankar.edgi01@gmail.com>
2018-11-10 10:28:27 +0530

    Complete project setup is done

---
+++ onlineshopping/src/main/java/com/shs/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.shs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value= {"/","/index","/home"})
+	public ModelAndView index() {
+		ModelAndView mv=new ModelAndView("page");
+		return mv;
+	}
+}
